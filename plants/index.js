bb277e8a20aac84f6568cf91cf6739db1a44609d
Evgeniy Ermakov <iinegve_25@mail.ru>
2022-12-21 22:49:42 +0300

    feat: add interactive hamburger menu

--- plants/index.js
+++ plants/index.js
@@ -7,4 +7,50 @@ console.log(`
 4: "12" \n
 5: "20" \n
 Score: "110"
-`)
+`);
+
+// Hamburger menu
+
+function HamburgerMenu(){
+    const btnHamburger = document.querySelector('.hamburger_menu');
+    const navigation = document.querySelector('.navigation')
+    const body = document.querySelector('body');
+    const header = document.querySelector('header');
+    const container = document.querySelector('.container');
+
+    const clickOnBurger = () => {
+        body.classList.toggle('inactive');
+        container.classList.toggle('inactive');
+        header.classList.toggle('inactive');
+        btnHamburger.classList.toggle('active');
+        navigation.classList.toggle('hamburger');
+    }
+
+    const closeMenuClickOnLinks = (e) => {
+        const links = navigation.querySelectorAll('a');
+        links.forEach(link => {
+            if (link === e.target){
+                clickOnBurger();
+                navigation.removeEventListener('click', closeMenuClickOnLinks);
+            }
+        })
+    }
+
+    const closeMenuClickOnBackground = (e) => {
+        if (e.target === container) {
+           clickOnBurger()
+           container.removeEventListener('click', closeMenuClickOnBackground)
+        }
+    }
+
+    const openMenuMobile = () => {
+        navigation.addEventListener('click', closeMenuClickOnLinks);
+        container.addEventListener('click', closeMenuClickOnBackground);
+        clickOnBurger();
+    }
+
+    btnHamburger.addEventListener('click', openMenuMobile)
+
+}
+
+HamburgerMenu()
