3342f80bacd156a8cda77c6ec22b30d5caa05d7e
Duxcoder <iinegve_25@mail.ru>
2023-10-02 22:48:38 +0300

    refactor: prettier

--- plants/index.js
+++ plants/index.js
@@ -1,137 +1,133 @@
-'use strict'
-
-console.log(`
-1: "50" \n
-2: "50" \n
-3: "25" \n
-Score: "125"
-`);
+'use strict';
 
 // Hamburger menu
 
-function HamburgerMenu(){
-    const btnHamburger = document.querySelector('.hamburger_menu');
-    const navigation = document.querySelector('.navigation')
-    const body = document.querySelector('body');
-    const header = document.querySelector('header');
-    const container = document.querySelector('.container');
+function HamburgerMenu() {
+  const btnHamburger = document.querySelector('.hamburger_menu');
+  const navigation = document.querySelector('.navigation');
+  const body = document.querySelector('body');
+  const header = document.querySelector('header');
+  const container = document.querySelector('.container');
 
-    const clickOnBurger = () => {
-        body.classList.toggle('inactive');
-        container.classList.toggle('inactive');
-        header.classList.toggle('inactive');
-        btnHamburger.classList.toggle('active');
-        navigation.classList.toggle('hamburger');
-    }
+  const clickOnBurger = () => {
+    body.classList.toggle('inactive');
+    container.classList.toggle('inactive');
+    header.classList.toggle('inactive');
+    btnHamburger.classList.toggle('active');
+    navigation.classList.toggle('hamburger');
+  };
 
-    const closeMenuClickOnLinks = (e) => {
-        const links = navigation.querySelectorAll('a');
-        links.forEach(link => {
-            if (link === e.target){
-                clickOnBurger();
-                navigation.removeEventListener('click', closeMenuClickOnLinks);
-            }
-        })
-    }
-
-    const closeMenuClickOnBackground = (e) => {
-        if (e.target === container) {
-           clickOnBurger()
-           container.removeEventListener('click', closeMenuClickOnBackground)
-        }
-    }
-
-    const openMenuMobile = () => {
-        navigation.addEventListener('click', closeMenuClickOnLinks);
-        container.addEventListener('click', closeMenuClickOnBackground);
+  const closeMenuClickOnLinks = (e) => {
+    const links = navigation.querySelectorAll('a');
+    links.forEach((link) => {
+      if (link === e.target) {
         clickOnBurger();
+        navigation.removeEventListener('click', closeMenuClickOnLinks);
+      }
+    });
+  };
+
+  const closeMenuClickOnBackground = (e) => {
+    if (e.target === container) {
+      clickOnBurger();
+      container.removeEventListener('click', closeMenuClickOnBackground);
     }
+  };
 
-    btnHamburger.addEventListener('click', openMenuMobile)
+  const openMenuMobile = () => {
+    navigation.addEventListener('click', closeMenuClickOnLinks);
+    container.addEventListener('click', closeMenuClickOnBackground);
+    clickOnBurger();
+  };
 
+  btnHamburger.addEventListener('click', openMenuMobile);
 }
 
-HamburgerMenu()
+HamburgerMenu();
 
 // Service choice
 
 const domBtnsService = document.querySelectorAll('.service__btn');
 const domCards = document.querySelectorAll('[data-card]');
 
-let activeBtns = []
+let activeBtns = [];
 const addToActiveBtns = (item) => {
-    if (activeBtns.length < 2) {
-        activeBtns.push(item)
-    }
-    if (activeBtns.length === 2) {
-       let arr = Array.from(domBtnsService)
-       arr.filter((elem) => !activeBtns.includes(elem))[0].classList.add('disabled')
-    }
-    activeBtns.forEach(elem => elem.classList.add('active'))
-}
+  if (activeBtns.length < 2) {
+    activeBtns.push(item);
+  }
+  if (activeBtns.length === 2) {
+    let arr = Array.from(domBtnsService);
+    arr.filter((elem) => !activeBtns.includes(elem))[0].classList.add('disabled');
+  }
+  activeBtns.forEach((elem) => elem.classList.add('active'));
+};
 const removeActiveBtns = (item) => {
-   item.classList.remove('active');
-   activeBtns = activeBtns.filter(elem => elem != item);
-   domBtnsService.forEach(elem => elem.classList.remove('disabled'));
-}
+  item.classList.remove('active');
+  activeBtns = activeBtns.filter((elem) => elem != item);
+  domBtnsService.forEach((elem) => elem.classList.remove('disabled'));
+};
 
 document.addEventListener('click', (e) => {
-    domBtnsService.forEach(item => {
-        if (e.target.name === item.name){
-            activeBtns.includes(item) ? removeActiveBtns(item) : addToActiveBtns(item);
-            if (activeBtns.length){
-                let arrCards = Array.from(domCards)
-                let arr = [];
-                activeBtns.forEach(btn => {arr = [...arr, ...arrCards.filter(card => card.dataset.card === btn.name)]})
-                domCards.forEach(elem => elem.classList.add('active'));
-                arr.forEach(elem => elem.classList.remove('active'))
-            } else {
-                domCards.forEach(elem => elem.classList.remove('active'))
-            }
-        }
-    }) 
-})
-
+  domBtnsService.forEach((item) => {
+    if (e.target.name === item.name) {
+      activeBtns.includes(item) ? removeActiveBtns(item) : addToActiveBtns(item);
+      if (activeBtns.length) {
+        let arrCards = Array.from(domCards);
+        let arr = [];
+        activeBtns.forEach((btn) => {
+          arr = [...arr, ...arrCards.filter((card) => card.dataset.card === btn.name)];
+        });
+        domCards.forEach((elem) => elem.classList.add('active'));
+        arr.forEach((elem) => elem.classList.remove('active'));
+      } else {
+        domCards.forEach((elem) => elem.classList.remove('active'));
+      }
+    }
+  });
+});
 
 // Accordion
 
-const domPrices = document.querySelector('.prices__content')
+const domPrices = document.querySelector('.prices__content');
 const domBtnsPrices = document.querySelectorAll('.btn__prices');
 let openedPriceCard = '';
 
 domPrices.addEventListener('mousedown', (e) => {
-    const target = e.target
-    console.dir(e.layerY)
-    domBtnsPrices.forEach(item => {
-        if ((item === target && e.layerY <= 52) || 
-        (target.parentElement.parentElement === item && e.layerY <= 52)
-        || target.parentElement === item && e.layerY <= 52) {
-            if (item === openedPriceCard) {
-                item.children[1].classList.remove('active'); 
-                item.classList.remove('active'); 
-                openedPriceCard = '';
-            } else {
-                domBtnsPrices.forEach(btn => {
-                    btn.children[1].classList.remove('active'); 
-                    btn.classList.remove('active');  
-                })
-                item.children[1].classList.add('active'); 
-                item.classList.add('active');
-                openedPriceCard = item;
-            }
-        }
-        if (e.target.className === 'btn__order'){
-            window.location = '#contacts';
-        }
-    })
-})
+  const target = e.target;
+  console.dir(e.layerY);
+  domBtnsPrices.forEach((item) => {
+    if (
+      (item === target && e.layerY <= 52) ||
+      (target.parentElement.parentElement === item && e.layerY <= 52) ||
+      (target.parentElement === item && e.layerY <= 52)
+    ) {
+      if (item === openedPriceCard) {
+        item.children[1].classList.remove('active');
+        item.classList.remove('active');
+        openedPriceCard = '';
+      } else {
+        domBtnsPrices.forEach((btn) => {
+          btn.children[1].classList.remove('active');
+          btn.classList.remove('active');
+        });
+        item.children[1].classList.add('active');
+        item.classList.add('active');
+        openedPriceCard = item;
+      }
+    }
+    if (e.target.className === 'btn__order') {
+      window.location = '#contacts';
+    }
+  });
+});
 
 domPrices.addEventListener('mousemove', (e) => {
-    if (e.layerY >= 52 && 'btn__order' != e.target.className) {e.target.style.cursor = 'auto'}
-    else {e.target.style.cursor = 'pointer'}
-
-})
-
+  if (e.layerY >= 52 && 'btn__order' != e.target.className) {
+    e.target.style.cursor = 'auto';
+  } else {
+    e.target.style.cursor = 'pointer';
+  }
+});
 
 // Custom select
 
@@ -144,64 +140,63 @@ const cityCard = document.querySelector('.city__card');
 const fieldCity = cityCard.querySelector('.city_field');
 const fieldPhone = cityCard.querySelector('.phone_field');
 const fieldAddress = cityCard.querySelector('.address_field');
-const btnCall  = cityCard.querySelector('.call_href')
+const btnCall = cityCard.querySelector('.call_href');
 const cardsData = [
-    {
-        city: 'Yonkers, NY',
-        phone: '+1	914	678 0003',
-        address: '511 Warburton Ave'
-    },
-    {
-        city: 'Canandaigua, NY',
-        phone: '+1	585	393 0001',
-        address: '151 Charlotte Street'
-    },
-    {
-        city: 'Sherrill, NY',
-        phone: '+1	315	908 0004',
-        address: '14 WEST Noyes BLVD'
-    },
-    {
-        city: 'New York City',
-        phone: '+1	212	456 0002',
-        address: '9 East 91st Street'
-    }
+  {
+    city: 'Yonkers, NY',
+    phone: '+1	914	678 0003',
+    address: '511 Warburton Ave',
+  },
+  {
+    city: 'Canandaigua, NY',
+    phone: '+1	585	393 0001',
+    address: '151 Charlotte Street',
+  },
+  {
+    city: 'Sherrill, NY',
+    phone: '+1	315	908 0004',
+    address: '14 WEST Noyes BLVD',
+  },
+  {
+    city: 'New York City',
+    phone: '+1	212	456 0002',
+    address: '9 East 91st Street',
+  },
 ];
-let active = false
+let active = false;
 
 wrapper.addEventListener('click', (e) => {
-    if (e.target === domSelected || e.target.parentElement === domSelected) {
-        domOptionsWrapper.classList.add('active');
-        domSelected.classList.add('active');
-        cityCard.classList.remove('active');
-        domFarmer.classList.add('inactive');
-        if (active) {
-            domOptionsWrapper.classList.remove('active');
-            domSelected.classList.remove('active');
-            domSelected.textContent === 'City' ? cityCard.classList.remove('active') : cityCard.classList.add('active') 
-            domSelected.textContent === 'City' ? domFarmer.classList.remove('inactive') : null
-
-        }
-        active = !active;
+  if (e.target === domSelected || e.target.parentElement === domSelected) {
+    domOptionsWrapper.classList.add('active');
+    domSelected.classList.add('active');
+    cityCard.classList.remove('active');
+    domFarmer.classList.add('inactive');
+    if (active) {
+      domOptionsWrapper.classList.remove('active');
+      domSelected.classList.remove('active');
+      domSelected.textContent === 'City'
+        ? cityCard.classList.remove('active')
+        : cityCard.classList.add('active');
+      domSelected.textContent === 'City' ? domFarmer.classList.remove('inactive') : null;
     }
-    if (e.target.classList.contains('option')) {
-        cardsData.forEach(card => {
-            if (e.target.textContent === card.city ){
-                spanSelected.style.fontSize = '16px';
-                // domSelected.style.paddingLeft = '34px';
-                spanSelected.textContent = card.city;
-                    fieldCity.textContent = card.city;
-                    fieldPhone.textContent = card.phone;
-                    fieldAddress.textContent = card.address;
-                    btnCall.href = `tel:${card.phone.replace(/\s/g, '')}`;
-                domOptionsWrapper.classList.remove('active');
-                domSelected.classList.remove('active');
-                domSelected.classList.add('activeAlways');
-                wrapper.classList.add('activeAlways')
-                cityCard.classList.add('active');
-            }
-        })
-        active = !active;   
-    }
-})
-
+    active = !active;
+  }
+  if (e.target.classList.contains('option')) {
+    cardsData.forEach((card) => {
+      if (e.target.textContent === card.city) {
+        spanSelected.style.fontSize = '16px';
+        spanSelected.textContent = card.city;
+        fieldCity.textContent = card.city;
+        fieldPhone.textContent = card.phone;
+        fieldAddress.textContent = card.address;
+        btnCall.href = `tel:${card.phone.replace(/\s/g, '')}`;
+        domOptionsWrapper.classList.remove('active');
+        domSelected.classList.remove('active');
+        domSelected.classList.add('activeAlways');
+        wrapper.classList.add('activeAlways');
+        cityCard.classList.add('active');
+      }
+    });
+    active = !active;
+  }
+});
